a6b6b62b672866ada86eb0485312372e422413a1
pnguyen <9phuc.nguyen6@gmail.com>
2023-07-09 23:44:34 -0500

    fixed formula vars

--- calculate_rpm.py
+++ calculate_rpm.py
@@ -3,7 +3,12 @@
 gear_ratio_map = {1: 5.0867, 2: 2.991, 3: 2.035, 4: 1.594, 5: 1.286, 6: 1.000}
 
 def calculate_rpm(mph, gear):
-    rpm = mph / (1 / gear_ratio_map.get(gear)) / (1 / 2.866) / (1 / 830) / (60)
+    final_drive_ratio = 2.866
+    transmission_ratio = gear_ratio_map.get(gear)
+    tyre_rev_per_mi = 830
+    min_per_hr = 60
+
+    rpm = mph / (1 / transmission_ratio) / (1 / final_drive_ratio) / (1 / tyre_rev_per_mi) / min_per_hr
     print(rpm)
 
 
