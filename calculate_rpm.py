05f67758049da4baf946f6e29d7cb674c06659bf
Jimmy Nguyen <9phuc.nguyen6@gmail.com>
2023-07-10 00:44:02 -0500

    Refactor vars and old py lib changes

--- calculate_rpm.py
+++ calculate_rpm.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+# import obd
 
 gear_ratio_map = {1: 5.0867, 2: 2.991, 3: 2.035, 4: 1.594, 5: 1.286, 6: 1.000}
 
 def calculate_rpm(mph, gear, tyre_width_mm, tyre_sidewall_in, rim_diameter_in):
     # Ratios
     final_drive_ratio = 2.866
-    transmission_ratio = gear_ratio_map.get(gear)
+    transmission_gear_ratio = gear_ratio_map.get(gear)
     final_gear_ratio = gear_ratio_map.get(6)
 
     # Wheel/Tyre specs
@@ -18,12 +19,18 @@ def calculate_rpm(mph, gear, tyre_width_mm, tyre_sidewall_in, rim_diameter_in):
     tyre_rev_per_mi = (inches_per_ft * ft_per_mi) / tyre_circumference
     min_per_hr = 60
 
-    
-    rpm = mph / (final_gear_ratio / transmission_ratio) / (final_gear_ratio / final_drive_ratio) / (final_gear_ratio / tyre_rev_per_mi) / min_per_hr
+    # Calculate RPM
+    rpm = mph / (final_gear_ratio / transmission_gear_ratio) / (final_gear_ratio / final_drive_ratio) / (final_gear_ratio / tyre_rev_per_mi) / min_per_hr
     print(rpm)
 
 def convert_to_inches(mm):
     return mm / 25.4
+
+def conenct_to_obd2():
+    ports = obd.scan_serial()      # return list of valid USB or RF ports
+    connection = obd.OBD(ports[0]) # connect to the first port in the list
+    if connection.status() == OBDStatus.CAR_CONNECTED:
+        query = connection.query(obd.commands.RPM) # current RPM. TODO : Need to calculate differential and display on LCD
 
 if __name__ == "__main__":
     calculate_rpm(36.2, 1, 205, 45, 17)
